22ae8b76c384e3e7b68d87de010ebc31500cabc9
60manojkumar@gmail.com <Motivated@2019>
2022-10-27 08:21:45 +0530

    Database TABLE

--- src/main/java/com/example/demospringboot/model/Student.java
+++ src/main/java/com/example/demospringboot/model/Student.java
@@ -1,10 +1,27 @@
 package com.example.demospringboot.model;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity(name = "STUDENT_8AM_AUGUST")  // STEP 1
 public class Student {
 
+    @Id  // it make this column as primary key
+    @GeneratedValue  // STEP 2 Automatically database will increase this value and use it
+    long id;
+
     String name;
+    @Column(name = "STD_AGE") // if we want to change name of column
     int age;
     String section;
+
+    //STEP 3 Default Constructor
+
+    public Student() {
+    }
+
 
     // parameterized constructor
 
